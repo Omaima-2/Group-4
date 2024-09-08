ae14a1c36af064836dd4936ada1f2260948b6eba
Omaima-2 <146462203+Omaima-2@users.noreply.github.com>
2024-09-08 09:39:07 +0300

    uddf

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,6 +6,7 @@
 // @dart = 3.0
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:geolocator_android/geolocator_android.dart';
 import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
 import 'package:google_sign_in_android/google_sign_in_android.dart';
 import 'package:image_picker_android/image_picker_android.dart';
@@ -13,6 +14,7 @@ import 'package:path_provider_android/path_provider_android.dart';
 import 'package:shared_preferences_android/shared_preferences_android.dart';
 import 'package:url_launcher_android/url_launcher_android.dart';
 import 'package:video_player_android/video_player_android.dart';
+import 'package:geolocator_apple/geolocator_apple.dart';
 import 'package:google_maps_flutter_ios/google_maps_flutter_ios.dart';
 import 'package:google_sign_in_ios/google_sign_in_ios.dart';
 import 'package:image_picker_ios/image_picker_ios.dart';
@@ -21,14 +23,13 @@ import 'package:shared_preferences_foundation/shared_preferences_foundation.dart
 import 'package:url_launcher_ios/url_launcher_ios.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
 import 'package:file_selector_linux/file_selector_linux.dart';
-import 'package:flutter_keyboard_visibility_linux/flutter_keyboard_visibility_linux.dart';
 import 'package:image_picker_linux/image_picker_linux.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
 import 'package:shared_preferences_linux/shared_preferences_linux.dart';
 import 'package:url_launcher_linux/url_launcher_linux.dart';
 import 'package:file_selector_macos/file_selector_macos.dart';
-import 'package:flutter_keyboard_visibility_macos/flutter_keyboard_visibility_macos.dart';
+import 'package:geolocator_apple/geolocator_apple.dart';
 import 'package:google_sign_in_ios/google_sign_in_ios.dart';
 import 'package:image_picker_macos/image_picker_macos.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
@@ -36,7 +37,6 @@ import 'package:shared_preferences_foundation/shared_preferences_foundation.dart
 import 'package:url_launcher_macos/url_launcher_macos.dart';
 import 'package:video_player_avfoundation/video_player_avfoundation.dart';
 import 'package:file_selector_windows/file_selector_windows.dart';
-import 'package:flutter_keyboard_visibility_windows/flutter_keyboard_visibility_windows.dart';
 import 'package:image_picker_windows/image_picker_windows.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
@@ -49,6 +49,15 @@ class _PluginRegistrant {
   @pragma('vm:entry-point')
   static void register() {
     if (Platform.isAndroid) {
+      try {
+        GeolocatorAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`geolocator_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         GoogleMapsFlutterAndroid.registerWith();
       } catch (err) {
@@ -113,6 +122,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isIOS) {
+      try {
+        GeolocatorApple.registerWith();
+      } catch (err) {
+        print(
+          '`geolocator_apple` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         GoogleMapsFlutterIOS.registerWith();
       } catch (err) {
@@ -187,15 +205,6 @@ class _PluginRegistrant {
       }
 
       try {
-        FlutterKeyboardVisibilityPluginLinux.registerWith();
-      } catch (err) {
-        print(
-          '`flutter_keyboard_visibility_linux` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-      }
-
-      try {
         ImagePickerLinux.registerWith();
       } catch (err) {
         print(
@@ -251,10 +260,10 @@ class _PluginRegistrant {
       }
 
       try {
-        FlutterKeyboardVisibilityPluginMacos.registerWith();
+        GeolocatorApple.registerWith();
       } catch (err) {
         print(
-          '`flutter_keyboard_visibility_macos` threw an error: $err. '
+          '`geolocator_apple` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
@@ -319,15 +328,6 @@ class _PluginRegistrant {
       } catch (err) {
         print(
           '`file_selector_windows` threw an error: $err. '
-          'The app may not function as expected until you remove this plugin from pubspec.yaml'
-        );
-      }
-
-      try {
-        FlutterKeyboardVisibilityPluginWindows.registerWith();
-      } catch (err) {
-        print(
-          '`flutter_keyboard_visibility_windows` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
